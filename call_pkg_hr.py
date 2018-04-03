86c3d919a9386ac39312df4a212b56cfdd8136a8
Tim King <timcking@gmail.com>
2018-04-03 04:17:52 +0100

    Added calls to all pkg_hr procedures

--- call_pkg_hr.py
+++ call_pkg_hr.py
@@ -2,19 +2,28 @@ import sys
 import pkg_hr
 
 if __name__ == "__main__":
+    """
     if len(sys.argv) <> 2:
         print 'Usage: ' + sys.argv[0] + ' department_id'
         sys.exit(1)
     else:
         l_deptid = int(sys.argv[1])
+    """
 
     hrq = pkg_hr.pkg_hr()
 
-    # l_count = hrq.get_employee_count(l_deptid)
-    # print int(l_count)
-    # sql = "SELECT last_name FROM employees WHERE last_name ='King'"
+    """ add_department
+    l_deptid = hrq.add_department('Cloud Heads', '200', '1700')
+    print(l_deptid)
+    """
 
+    """ employee_count
+    l_count = hrq.get_employee_count('30')
+    print int(l_count)
+    sql = "SELECT last_name FROM employees WHERE last_name ='King'"
+    """
+
+    """ find_employees """
     empArr = []
-
     empArr = hrq.find_employees('King')
     print empArr
